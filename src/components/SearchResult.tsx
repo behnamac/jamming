9ec6584f533714748daf52625b0419c31da4d994
Behnam <behnam.sep@gmail.com>
2025-03-09 15:05:17 +0100

    fix track

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,29 +1,19 @@
 import React from "react";
 import Track from "./Track";
 import songs from "../stores/data";
+import Table from "./ui/table";
 
 function SearchResult() {
   return (
-    <div className="w-1/2 bg-purple-900 rounded-t-xl">
-      <div className="w-full border-b-2 border-gray-700 bg-purple-800 rounded-t-xl">
-        <h1 className="ml-5 py-2 text-3xl font-bold text-amber-50">Result</h1>
+    <Table title="Result">
+      <div className="ml-2 p-2 text-xl">
+        {songs.map((song) => (
+          <div className="flex w-full border-b-2 border-purple-950">
+              <Track songName={song.song} singer={song.singer} />
+          </div>
+        ))}
       </div>
-      <div className="bg--500 h-full w-full overflow-auto bg-purple-900 py-1">
-        <ul className="ml-2 p-2 text-xl">
-          {songs.map((song) => (
-            <div className="flex w-full border-b-2 border-purple-950">
-              <li className="flex w-full flex-col p-3 text-amber-50">
-                {song.singer}
-                <span className="text-sm text-gray-400">{song.song}</span>
-              </li>
-              <button className="m-2 p-2 text-gray-200 hover:cursor-pointer hover:text-gray-300">
-                +
-              </button>
-            </div>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </Table>
   );
 }
 
--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -1,13 +1,17 @@
-
 interface props {
   songName: string;
   singer: string;
 }
 function Track({ songName, singer }: props) {
   return (
-    <div className="flex flex-col">
-      {songName}
-      {singer}
+    <div className="flex w-full p-3 text-amber-50">
+      <div className="flex w-full flex-col p-3 text-amber-50">
+        {songName}
+        <span className="text-sm text-gray-300">{singer}</span>
+      </div>
+      <button className="m-2 p-2 text-gray-200 hover:cursor-pointer hover:text-gray-300">
+        +
+      </button>
     </div>
   );
 }
